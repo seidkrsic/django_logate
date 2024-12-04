55e32bde34803223112f2365e0f610b6be2359c0
seidkrsic <seid.krsic@gmail.com>
2024-12-04 13:57:24 +0100

    decembar2024

---
+++ project1/newyear/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+import datetime 
+
+# Create your views here.
+
+
+def index(request): 
+    now = datetime.datetime.now()
+    return render(request, "newyear/index.html", {
+        # "newyear" : now.day == 1 and now.month == 1,
+        "newyear" : True,
+    })
